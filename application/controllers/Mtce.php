f81f57590998670d9fe631da57d46e0c34506846
maennis <mark.andrew.ennis@gmail.com>
2017-10-22 12:03:21 -0700

    Closes #4. Added Maintenance view and controller

---
+++ application/controllers/Mtce.php
@@ -0,0 +1,27 @@
+<?php
+/**
+*  The controller for the index page.
+*/
+class Mtce extends Application {
+
+    public function index()
+    {
+        $this->data['pagetitle'] = 'TODO List Maintenance';
+        $tasks = $this->tasks->all();
+        // substitute the status name
+        foreach ($tasks as $task)
+            if (!empty($task->status))
+                $task->status = $this->app->status($task->status);
+
+        // build the task presentation output
+        $result = '';
+        foreach ($tasks as $task)
+            $result .= $this->parser->parse('oneitem',(array)$task,true);
+
+        // and then pass them on
+        $this->data['display_tasks'] = $result;
+        $this->data['pagebody'] = 'itemlist';
+        $this->render();
+    }
+
+}
