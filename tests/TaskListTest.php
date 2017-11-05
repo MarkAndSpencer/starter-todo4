f5dc7054d4df532f68344d825a841abe4df64cdf
maennis <mark.andrew.ennis@gmail.com>
2017-11-05 13:13:50 -0800

    Fixed TaskListTest to work with higher versions of PHPUnit

--- tests/TaskListTest.php
+++ tests/TaskListTest.php
@@ -1,25 +1,28 @@
-    <?php
-    class TaskListTest extends PHPUnit_Framework_TestCase
+<?php
+
+use PHPUnit\Framework\TestCase;
+
+class TaskListTest extends TestCase
+{
+
+    private $CI;
+
+    public function setUp()
     {
-
-        private $CI;
-
-        public function setUp()
-        {
-            $this->CI = &get_instance();
-        }
-        public function testGetPost()
-        {
-            $completedCount = 0;
-            $this->CI->load->model('tasks');
-            $taskList = $this->CI->tasks->getAllTasks();
-            foreach($taskList as $task) {
-                if ($task->status == 2) {
-                    $completedCount += 1;
-                } else {
-                    $completedCount -= 1;
-                }
-            }
-            $this->assertTrue($completedCount < 0);
-        }
+        $this->CI = &get_instance();
     }
+    public function testTaskList()
+    {
+        $completedCount = 0;
+        $this->CI->load->model('tasks');
+        $taskList = $this->CI->tasks->getAllTasks();
+        foreach($taskList as $task) {
+            if ($task->status == 2) {
+                $completedCount += 1;
+            } else {
+                $completedCount -= 1;
+            }
+        }
+        $this->assertTrue($completedCount < 0);
+    }
+}
